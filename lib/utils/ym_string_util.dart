d70d27415fa2ffc1f543c70755b785962a050d46
zhaoqipeng <396196516@qq.com>
2021-06-08 10:22:51 +0800

    优化标题栏和增加姓名增加星号的字符串函数

--- lib/utils/ym_string_util.dart
+++ lib/utils/ym_string_util.dart
@@ -2,15 +2,34 @@
 
 class YmStringUtil {
 
-
+  ///去掉金额的.00
   static toStringMoney(double money) {
        String moneyNew = money.toStringAsFixed(2);
        return moneyNew.replaceAll(".00", "");
   }
 
+  ///判断是否为手机号
   static isMobile(String mobile){
     RegExp exp = RegExp(r'^((13[0-9])|(14[0-9])|(15[0-9])|(16[0-9])|(17[0-9])|(18[0-9])|(19[0-9]))\d{8}$');
     return exp.hasMatch(mobile);
+  }
+
+
+  ///将姓名变为*号
+  static String formatStarName(String name) {
+    String newStr;
+    if (name.length == 2) {
+      newStr = "*" + name.substring(1, 2);
+    } else if (name.length == 3) {
+      newStr = name.substring(0, 1) + "*" + name.substring(2);
+    } else if (name.length == 4) {
+      newStr = name.substring(0, 2) + "*" + name.substring(3);
+    }  else if (name.length > 4) {
+      newStr = name.substring(0, 2) + "**" + name.substring(4);
+    } else {
+      newStr = name;
+    }
+    return newStr;
   }
 
 }
--- lib/widgets/AppBar/ym_app_bar.dart
+++ lib/widgets/AppBar/ym_app_bar.dart
@@ -6,7 +6,7 @@ class YmAppBar extends StatelessWidget {
 
   final String title;                            // 标题名称
   final List<Color> background;                  // 背景颜色
-  final int barHeight = 80;                      // 标题栏高度
+  final double barHeight = 56;                   // 标题栏高度
   final Function() backAction;                   // 返回
   final Color titleTextColor;                    // 文本标题的颜色
 
@@ -19,35 +19,46 @@ class YmAppBar extends StatelessWidget {
     final double statusBarHeight = MediaQuery.of(context).padding.top;
     return Container(
       padding: EdgeInsets.only(top: statusBarHeight,left:10),
-      height: barHeight + 0.0,
       decoration: BoxDecoration(
         gradient: LinearGradient(
             colors: background
         ),
       ),
-      child:new Row(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          IconButton(
-            icon: new Icon(Icons.arrow_back_ios),
-            color: this.titleTextColor,
-            onPressed: backAction,
-            iconSize: 28,
+          Container(
+            width:barHeight,
+            height:barHeight,
+            child:IconButton(
+              icon: new Icon(Icons.arrow_back_ios),
+              color: this.titleTextColor,
+              onPressed: backAction,
+              iconSize: 28,
+            ),
           ),
 
           Expanded(
+            flex: 1,
             child: Container(
-               padding: EdgeInsets.only(right:60),
-               child:Text(
-                 title,
-                 textAlign:TextAlign.center,
-                 style: TextStyle(
-                   color: this.titleTextColor,
-                   fontSize: 20,
-                   fontWeight: FontWeight.w400,    //字体宽度
-                 ),
-               ),
-          )
+              child: Text(
+                title,
+                textAlign:TextAlign.center,
+                style: TextStyle(
+                  color: this.titleTextColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,    //字体宽度
+                ),
+              ),
+            ),
           ),
+
+          Container(
+            width:barHeight,
+            height:barHeight,
+          ),
+
         ],
       ),
     );
